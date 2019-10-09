21e5573987348329fb1df25054dcffdaec339eff
user <allionhouse2@gmail.com>
2019-10-09 16:59:58 -0700

    edit

--- sklearn_ex.py
+++ sklearn_ex.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import GridSearchCV #more pipeline
 from sklearn.metrics import mean_squared_error, r2_score #eval metrics
 from sklearn.externals import joblib #persists model, pickle package to store large numpy arrays
 
-
+#edit
 dataset_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
 data = pd.read_csv(dataset_url) #pandas gets csv data from the web? neat
 print(data)
